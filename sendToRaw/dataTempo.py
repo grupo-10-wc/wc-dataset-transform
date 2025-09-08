d0b5e25e091ba308db13c02b19a1e0dcca7bad3f
Rafaqwert <rafael.rrodrigues@sptech.school>
2025-09-08 20:20:01 -0300

    feat: tratamento de dados meteorologicos

--- sendToRaw/dataTempo.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def download_pdf(url="https://igce.rc.unesp.br/Home/ComissaoSupervisora-old/ConservacaodeEnergiaCICE/tabela_consumo.pdf", folder="./sendToRaw/files", filename="consumoAparelho.pdf"):
-    filepath = os.path.join(folder, filename)
-    response = requests.get(url)
-    with open(filepath, "wb") as f:
-        f.write(response.content)
-    return filename

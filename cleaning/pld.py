ede5ad255764c31df95eca19bfbe24f193a361bf
raizen-arthurmoraes2 <arthur.moraes2@raizen.com>
2025-09-07 21:38:16 -0300

    feat: terminando cleaning de pld

--- cleaning/pld.py
+++ cleaning/pld.py
@@ -5,11 +5,15 @@ import requests
 import pandas as pd
 from io import StringIO
 
-# TODO: SUBSTITUIR ESSA LEITURA PELA LEITURA NO S3
+
 
 def read_from_raw(
     save_date:datetime.datetime=datetime.datetime.now()
 ):
+    # ------------------------------------------------
+    # TODO: SUBSTITUIR ESSA LEITURA PELA LEITURA NO S3
+    # ------------------------------------------------
+
     res = requests.get("https://pda-download.ccee.org.br/korJMXwpSLGyVlpRMQWduA/content")
     res.raise_for_status()
     
@@ -17,13 +21,17 @@ def read_from_raw(
     
     return texto
 
-def teste():
-    texto = read_from_raw()
-    df = pd.read_csv(StringIO(texto), delimiter=';')
-    pdb.set_trace()
-    df['DIA']
-    df['datahora'] = df['MES_REFERENCIA'].astype(str) + df['DIA'].astype(str) + df['HORA'].astype(str)
+def normalizar_datahora_pld(texto_csv: str = read_from_raw()):
+    df = pd.read_csv(StringIO(texto_csv), delimiter=';')
+    
+    df['DIA'] = df['DIA'].apply(lambda x: f"{x:02.0f}")
+    df['HORA'] = df['HORA'].apply(lambda x: f"{x:02.0f}")
+    df['DATAHORA'] = df.apply(
+        lambda row: datetime.datetime.strptime(f"{row['MES_REFERENCIA']}{row['DIA']}{row['HORA']}", "%Y%m%d%H"),
+        axis=1
+    )
+    df.drop(columns=['MES_REFERENCIA', 'DIA', 'HORA'], inplace=True)
     return df
 
 if __name__ == "__main__":
-    teste()
+    normalizar_datahora_pld()

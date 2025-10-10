0e4859e05e3d35755fb79dfa6d14eac41181c048
raizen-arthurmoraes <arthur.moraes2@raizen.com>
2025-10-09 21:12:06 -0300

    header no consumo e removendo coluna do dataset de pld

--- cleaning/consumoAparelho.py
+++ cleaning/consumoAparelho.py
@@ -17,8 +17,7 @@ def remove_accents(text):
 def pdf_to_csv(pdf_path, csv_path="consumoAparelho.csv"):
     with pdfplumber.open(pdf_path) as pdf, open(csv_path, "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        # Write header
-        #writer.writerow(["Aparelho", "Potência (W)", "Dias/mês", "Uso/dia", "unidade", "Consumo mensal (kWh)"])
+        writer.writerow(['aparelho', 'potencia', 'dias', 'utilizacao', 'medida', 'consumo'])
 
         for page in pdf.pages:
             text = page.extract_text()
@@ -38,11 +37,9 @@ def pdf_to_csv(pdf_path, csv_path="consumoAparelho.csv"):
                     utilizacao = "1" if parts[-3] == "-" else parts[-3]
                     medida = "h" if parts[-2] == "-" else parts[-2]
                     consumo = parts[-1]
-                    #print([aparelho, potencia, dias, utilizacao, medida, consumo])
                     writer.writerow([aparelho, potencia, dias, utilizacao, medida, consumo])
 
 if __name__ == "__main__":
-    url = "https://igce.rc.unesp.br/Home/ComissaoSupervisora-old/ConservacaodeEnergiaCICE/tabela_consumo.pdf"  # your PDF link here
+    url = "https://igce.rc.unesp.br/Home/ComissaoSupervisora-old/ConservacaodeEnergiaCICE/tabela_consumo.pdf" 
     pdf_file = download_pdf(url)
     pdf_to_csv(pdf_file)
-    print("✅ PDF text extracted into CSV.")
--- cleaning/pld.py
+++ cleaning/pld.py
@@ -10,9 +10,6 @@ from io import StringIO
 def read_from_raw(
     save_date:datetime.datetime=datetime.datetime.now()
 ):
-    # ------------------------------------------------
-    # TODO: SUBSTITUIR ESSA LEITURA PELA LEITURA NO S3
-    # ------------------------------------------------
 
     res = requests.get("https://pda-download.ccee.org.br/korJMXwpSLGyVlpRMQWduA/content")
     res.raise_for_status()
@@ -21,7 +18,7 @@ def read_from_raw(
     
     return texto
 
-def normalizar_datahora_pld(texto_csv: str = read_from_raw()):
+def normalizar_pld(texto_csv: str = read_from_raw()):
     df = pd.read_csv(StringIO(texto_csv), delimiter=';')
     
     df['DIA'] = df['DIA'].apply(lambda x: f"{x:02.0f}")
@@ -30,8 +27,8 @@ def normalizar_datahora_pld(texto_csv: str = read_from_raw()):
         lambda row: datetime.datetime.strptime(f"{row['MES_REFERENCIA']}{row['DIA']}{row['HORA']}", "%Y%m%d%H"),
         axis=1
     )
-    df.drop(columns=['MES_REFERENCIA', 'DIA', 'HORA'], inplace=True)
+    df.drop(columns=['MES_REFERENCIA', 'DIA', 'HORA', 'PERIODO_COMERCIALIZACAO'], inplace=True)
     return df
 
 if __name__ == "__main__":
-    normalizar_datahora_pld()
+    normalizar_pld()
